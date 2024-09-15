42573ecd38e19436b82b61ca9b599d9b47958c6b
llhea <llheadlam@hotmail.com>
2024-09-15 12:56:26 +0200

    Add models for order processing

---
+++ Models/Order.cs
@@ -0,0 +1,13 @@
+﻿namespace OrderProcessor.Models
+{
+    public class Order
+    {
+        public string OrderNumber { get; set; } = string.Empty;
+        public string OrderDate { get; set; } = string.Empty;
+        public string CompanyCode { get; set; } = string.Empty;
+        public string PosNumber { get; set; } = string.Empty;
+        public string Address { get; set; } = string.Empty;
+        public string Phone { get; set; } = string.Empty;
+        public List<OrderRow> Rows { get; set; } = [];
+    }
+}
---
+++ Models/OrderRow.cs
@@ -0,0 +1,13 @@
+﻿namespace OrderProcessor.Models
+{
+    public class OrderRow
+    {
+        public string ArticleNumber { get; set; } = string.Empty;
+        public string Size { get; set; } = string.Empty;
+        public int Quantity { get; set; }
+        public decimal PriceIncVat { get; set; }
+        public decimal PriceExcVat { get; set; }
+        public string Color { get; set; } = string.Empty;
+        public string Reference { get; set; } = string.Empty;
+    }
+}
