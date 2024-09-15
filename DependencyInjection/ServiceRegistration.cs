6af514347971e25cdd89e5b50de1121a1e5fe08e
llhea <llheadlam@hotmail.com>
2024-09-15 13:01:21 +0200

    Add service registration setup

---
+++ DependencyInjection/ServiceRegistration.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using OrderProcessor.Interfaces;
+using OrderProcessor.Services;
+using System.Text.Encodings.Web;
+using System.Text.Json;
+
+namespace OrderProcessor.DependencyInjection
+{
+    public static class ServiceRegistration
+    {
+        public static void ConfigureServices(HostBuilderContext context, IServiceCollection services)
+        {
+            services.AddSingleton(new JsonSerializerOptions
+            {
+                WriteIndented = true,
+                Encoder = JavaScriptEncoder.UnsafeRelaxedJsonEscaping
+            });
+
+            services.AddSingleton<IOrderSaver, OrderSaver>();
+            services.AddSingleton<IOrderParser, OrderParser>();
+            services.AddSingleton<FileProcessingService>();
+            services.AddLogging(configure => configure.AddConsole());
+        }
+    }
+}
